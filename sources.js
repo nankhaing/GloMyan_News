8e2aa09b9eb71fd75e1db22adf7d881425892f1c
nankhaing <nankhaing224@gmail.com>
2026-03-10 09:57:24 +0630

    Add sources for various news categories

---
+++ sources.js
@@ -0,0 +1,27 @@
+const sources = {
+
+myanmar:[
+"https://rss.app/feeds/myanmar.xml",
+"https://rss.app/feeds/irrawaddy.xml",
+"https://rss.app/feeds/mizzima.xml"
+],
+
+asean:[
+"https://rss.app/feeds/thailand.xml",
+"https://rss.app/feeds/vietnam.xml",
+"https://rss.app/feeds/singapore.xml"
+],
+
+world:[
+"https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+],
+
+tech:[
+"https://techcrunch.com/feed/"
+],
+
+sports:[
+"https://www.espn.com/espn/rss/news"
+]
+
+};
